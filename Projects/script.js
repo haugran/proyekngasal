5a133a0159ceca1103c4a05e800183041801c34d
haugran <irfananshr.id@gmail.com>
2024-10-02 23:22:06 +0800

    Update script.js

--- Projects/script.js
+++ Projects/script.js
@@ -11,9 +11,9 @@ const roles = [
 document.getElementById("generateButton").addEventListener("click", function() {
     const name = document.getElementById("nameInput").value;
     if (name === "") {
-        document.getElementById("roleResult").textContent = "Masukin nama Kamu!";
+        document.getElementById("roleResult").textContent = "Masukin nama kamu!";
     } else {
         const randomRole = roles[Math.floor(Math.random() * roles.length)];
-        document.getElementById("roleResult").textContent = `${name}, kamu: ${randomRole}`;
+        document.getElementById("roleResult").textContent = `${name}, role kamu adalah: ${randomRole}`;
     }
 });
