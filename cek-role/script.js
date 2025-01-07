3f7015081b9bfbafc970eabc9319e342b12de223
Irfan Anshari <irfananshr.id@gmail.com>
2025-01-07 15:31:36 +0800

    Update script.js

--- cek-role/script.js
+++ cek-role/script.js
@@ -66,38 +66,31 @@ const roles = [
     "Ancient Egyptian Testicle Slap"
     
 ];
-// Akses elemen-elemen HTML
 const generateButton = document.getElementById("generateButton");
 const nameInput = document.getElementById("nameInput");
 const roleResult = document.getElementById("roleResult");
 const loading = document.getElementById("loading");
 
-// Event listener untuk tombol "Cek"
 generateButton.addEventListener("click", function() {
     const name = nameInput.value;
 
     if (name === "") {
         roleResult.textContent = "Masukin nama kamu!";
     } else {
-        // Menghapus hasil sebelumnya & menampilkan animasi loading
         roleResult.textContent = ""; 
-        loading.style.display = "flex"; // Tampilan popup loading layout fleksibel
+        loading.style.display = "flex";
 
-        // Simulasi loading 3 detik
         setTimeout(function() {
-        // Memilih salah satu role di  daftar secara acak saat klik tombol "Cek"
             const randomRole = roles[Math.floor(Math.random() * roles.length)];
             roleResult.textContent = `${name}, role kamu adalah: ${randomRole}`;
             
-            // Menyembunyikan popup loading setelah 3 detik
             loading.style.display = "none";
         }, 3000);
     }
 });
 
-// Event listener untuk tombol Enter di keyboard pada input field
 nameInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        generateButton.click(); // Trigger tombol "Cek" saat tekan Enter di keyboard
+        generateButton.click();
     }
 });
